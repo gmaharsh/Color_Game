9d7b01e49068b888b2d9af19b38b6e9a9fc3fcf6
Maharsh Gheewala <gmaharsh@users.noreply.github.com>
2017-01-04 14:32:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,67 @@
-var colors = generateRandomColors(6);
-
+var numSquares = 6;
+var colors = generateRandomColors(numSquares);
 var squares = document.querySelectorAll(".square");
-
 var pickedcolor = pickcolor();
-
 var colorDisplay = document.getElementById("colorDisplay");
-
 var messageDisplay = document.getElementById("message");
-
 var h1 = document.getElementById("heading")
-
 colorDisplay.textContent = pickedcolor;
+var reset = document.getElementById("newgame");
+var easy = document.getElementById("easy");
+var hard = document.getElementById("hard");
 
+easy.addEventListener("click",function(){
+    hard.classList.remove("selected");
+    easy.classList.add("selected");
+
+    numSquares = 3;
+    colors = generateRandomColors(numSquares);
+
+    pickedcolor = pickcolor();
+
+    colorDisplay.textContent = pickedcolor;
+
+    for(var i=0;i<squares.length;i++){
+    	if(colors[i]){
+            squares[i].style.background = colors[i];
+    	}else{
+    		squares[i].style.display = "none";
+    	}
+    }
+});
+
+hard.addEventListener("click",function(){
+    easy.classList.remove("selected");
+    hard.classList.add("selected");
+
+    numSquares = 6;
+    colors = generateRandomColors(numSquares);
+
+    pickedcolor = pickcolor();
+
+    colorDisplay.textContent = pickedcolor;
+
+    for(var i=0;i<squares.length;i++){
+    	if(colors[i]){
+            squares[i].style.background = colors[i];
+    		squares[i].style.display = "block";
+    }
+}
+});
+
+
+reset.addEventListener("click",function(){
+	colors = generateRandomColors(6);
+
+	pickedcolor = pickcolor();
+
+	colorDisplay.textContent = pickedcolor;
+
+	for(var i=0;i<squares.length;i++){
+		squares[i].style.background = colors[i];
+	}
+     h1.style.background = "#232323";
+});
 for(var i=0;i<squares.length;i++){
 	squares[i].style.background = colors[i];
 
@@ -19,7 +69,8 @@ for(var i=0;i<squares.length;i++){
 		var clickedColor = this.style.background;
 
 		if(clickedColor === pickedcolor){
-			messageDisplay.textContent = "Correct";
+			messageDisplay.textContent = " Correct!!!";
+			reset.textContent = "Play Again?";
 			changeColor(clickedColor);
 			h1.style.background = pickedcolor;
 		}
@@ -60,4 +111,8 @@ function randomcolor(){
 	var b = Math.floor(Math.random() * 256);
 
 	return "rgb(" + r +", " + g + ", " + b +")";
+}
+
+function reset(){
+
 }
