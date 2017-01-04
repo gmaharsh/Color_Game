36c5b9b79872e6183807fdce1848a7d805573ca6
Maharsh Gheewala <gmaharsh@users.noreply.github.com>
2017-01-04 16:17:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ reset.addEventListener("click",function(){
 	for(var i=0;i<squares.length;i++){
 		squares[i].style.background = colors[i];
 	}
-     h1.style.background = "#232323";
+     h1.style.background = "steelblue";
 });
 for(var i=0;i<squares.length;i++){
 	squares[i].style.background = colors[i];
