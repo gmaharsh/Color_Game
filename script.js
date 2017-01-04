e092951a97f6f515d138d7de789cfc2f534aab8a
Maharsh Gheewala <gmaharsh@users.noreply.github.com>
2017-01-04 13:19:10 +0530

    Updated Js

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-var colors = 
-["rgb(255, 0, 0)",
- "rgb(0, 0, 255)",
- "rgb(0, 255, 0)",
- "rgb(255, 0, 255)",
- "rgb(255, 255, 0)",
- "rgb(0, 255, 255)"
-]
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
 
@@ -14,6 +7,8 @@ var pickedcolor = pickcolor();
 var colorDisplay = document.getElementById("colorDisplay");
 
 var messageDisplay = document.getElementById("message");
+
+var h1 = document.getElementById("heading")
 
 colorDisplay.textContent = pickedcolor;
 
@@ -26,6 +21,7 @@ for(var i=0;i<squares.length;i++){
 		if(clickedColor === pickedcolor){
 			messageDisplay.textContent = "Correct";
 			changeColor(clickedColor);
+			h1.style.background = pickedcolor;
 		}
 		else
 		{
@@ -44,4 +40,24 @@ function changeColor(color){
 function pickcolor(){
 	var random = Math.floor(Math.random() * colors.length);
 	return colors[random];
+}
+
+function generateRandomColors(num){
+	var arr =[]
+    
+    for(var i=0;i<num;i++){
+    	arr.push(randomcolor());
+    }
+
+	return arr;
+}
+
+function randomcolor(){
+	var r = Math.floor(Math.random() * 256);
+
+	var g = Math.floor(Math.random() * 256);
+
+	var b = Math.floor(Math.random() * 256);
+
+	return "rgb(" + r +", " + g + ", " + b +")";
 }
